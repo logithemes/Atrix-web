cff15737abadb22f0fd70c8e56b3dcbd1f72fd12
lokishkumar36 <113993164+lokishkumar36@users.noreply.github.com>
2023-06-21 18:03:51 +0530

    addding

---
+++ azim.commonsupport.com/Atrix/assets/js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   
+
+    $(".booking-logo").click(function(){
+        $(".booking-itm").addClass("act"); 
+     });
+     
+     
+   $(".booking-itm .close1").click(function(){
+      $(".booking-itm").removeClass("act"); 
+   });
+     
+
+  });
